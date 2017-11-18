8e41b5fabc129bf5effb7ef5a088c037404bb8ec
Alex Sánchez <alxsnchez@gmail.com>
2017-11-18 12:44:17 +0100

    feat: deck details component

--- components/DeckDetails.js
+++ components/DeckDetails.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, TextInput, View, StyleSheet, TouchableOpacity} from 'react-native'
 
 class DeckDetails extends Component {
 
+    componentDidMount() {
+
+    }
+
     render() {
         const { navigation } = this.props
+        const { title, questions } = navigation.state.params
         return (
-            <Text>{navigation.state.params.title}</Text>
+            <View style={{flex: 1, padding: 40}}>
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                    <Text style={{fontSize: 32, textAlign: 'center'}}>{title}</Text>
+                    <Text style={{paddingTop: 8, fontSize: 18, textAlign: 'center'}}>{questions.length} cards</Text>
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.addCard}>
+                        <Text style={{fontSize: 16, textAlign: 'center', color: '#E91E63', fontWeight: 'bold'}}>ADD CARD</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.startQuiz}>
+                        <Text style={{fontSize: 16, textAlign: 'center', color: '#FFF', fontWeight: 'bold'}}>START QUIZ</Text>
+                    </TouchableOpacity>
+                </View>
+                
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    addCard: {
+        marginTop: 80,
+        marginBottom: 16,
+        padding: 16,
+        backgroundColor: '#fff',
+        borderRadius: 8
+    },
+    startQuiz: {
+        padding: 16,
+        backgroundColor: '#E91E63',
+        borderRadius: 8
+    }
+
+})
 
 export default DeckDetails
